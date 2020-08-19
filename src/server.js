c6ea149a066f9d8d49ccc7431a60f42ee8f30395
Patrick Brett <patrick@livebook.io>
2020-08-19 21:51:09 +1000

    Use env vars

--- src/server.js
+++ src/server.js
@@ -9,225 +9,229 @@ const SessionsManager = require('./SessionsManager');
 const gameHelper = new GameHelper();
 const sessionsManager = new SessionsManager(gameHelper);
 
+const path = require('path');
+const process = require('process');
+require('dotenv').config({ path: path.resolve(process.cwd(), `.env.${process.env.NODE_SERV_ENV}`) });
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://scatterbrain.tv");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+	res.header('Access-Control-Allow-Origin', 'https://scatterbrain.tv');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 app.get('/health-check', (req, res) => {
-  console.log("Health check succeeded")
-  res.status(200);
-  res.send('All good in the hood!')
-})
+	console.log('Health check succeeded');
+	res.status(200);
+	res.send('All good in the hood!');
+});
 
-http.listen(80, () => console.log('listening on port 80'));
+http.listen(process.env.SERV_PORT, () => console.log(`listening on port ${process.env.SERV_PORT}`));
 
 const io = require('socket.io')(http);
 
-io.on('connect', socket => {
-  socket.on('create-game', () => {
-    const gameCode = gameHelper.generateGameCode();
-    const hostCode = gameHelper.generateHostCode();
-  
-    sessionsManager.createGame(gameCode, hostCode, socket);
+io.on('connect', (socket) => {
+	socket.on('create-game', () => {
+		const gameCode = gameHelper.generateGameCode();
+		const hostCode = gameHelper.generateHostCode();
 
-    socket.emit('create-game-accepted', { gameCode, hostCode })
-  })
+		sessionsManager.createGame(gameCode, hostCode, socket);
 
-  socket.on('join-game', (data) => {
-    const { gameCode, playerName } = data;
-    try {
-      const { playerCode, isVip } = sessionsManager.joinGame(gameCode, playerName, socket)
-      console.log('joined as player', gameCode, playerName, playerCode);
-      console.log('Send join-game-accepted');
-      socket.emit('join-game-accepted', { playerCode, isVip })
+		socket.emit('create-game-accepted', { gameCode, hostCode });
+	});
 
-      const hostSocket = sessionsManager.getHostSocket(gameCode);
-      hostSocket.emit('new-player-joined', { playerName, isVip });
-    } catch(err) {
-      console.error(err)
-    }
-  })
+	socket.on('join-game', (data) => {
+		const { gameCode, playerName } = data;
+		try {
+			const { playerCode, isVip } = sessionsManager.joinGame(gameCode, playerName, socket);
+			console.log('joined as player', gameCode, playerName, playerCode);
+			console.log('Send join-game-accepted');
+			socket.emit('join-game-accepted', { playerCode, isVip });
 
-  socket.on('start-game', (data) => {
-    const { gameCode, playerCode } = data;
+			const hostSocket = sessionsManager.getHostSocket(gameCode);
+			hostSocket.emit('new-player-joined', { playerName, isVip });
+		} catch (err) {
+			console.error(err);
+		}
+	});
 
-    // Confirm that the player is VIP of the given game
-    const vip = sessionsManager.getVip(gameCode);
-    if (vip.playerCode !== playerCode) {
-      console.log(vip, playerCode);
-      console.error('Not the VIP!')
-      return;
-    }
+	socket.on('start-game', (data) => {
+		const { gameCode, playerCode } = data;
 
-    const hostSocket = sessionsManager.getHostSocket(gameCode);
-    const playerSockets = sessionsManager.getPlayerSockets(gameCode);
-    [hostSocket, ...playerSockets].forEach(socket => {
-      socket.emit('game-started');
-    })
-  })
+		// Confirm that the player is VIP of the given game
+		const vip = sessionsManager.getVip(gameCode);
+		if (vip.playerCode !== playerCode) {
+			console.log(vip, playerCode);
+			console.error('Not the VIP!');
+			return;
+		}
 
-  socket.on('reload-host', data => {
-    const { gameCode, hostCode } = data;
+		const hostSocket = sessionsManager.getHostSocket(gameCode);
+		const playerSockets = sessionsManager.getPlayerSockets(gameCode);
+		[hostSocket, ...playerSockets].forEach((socket) => {
+			socket.emit('game-started');
+		});
+	});
 
-    // Confirm that the host code is correct
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    if (game.hostCode !== hostCode) {
-      console.error('Host code incorrect', hostCode, game.hostCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('reload-host', (data) => {
+		const { gameCode, hostCode } = data;
 
-    // Replace the host socket
-    game.hostSocket = socket;
+		// Confirm that the host code is correct
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		if (game.hostCode !== hostCode) {
+			console.error('Host code incorrect', hostCode, game.hostCode);
+			// TODO: send error down socket
+			return;
+		}
 
-    const players = game.players.map(({ playerName, isVip, score })=> ({ playerName, isVip, score }));
+		// Replace the host socket
+		game.hostSocket = socket;
 
-    // Send the game info back to the host
-    socket.emit('reload-host-accepted', { players });
-  })
+		const players = game.players.map(({ playerName, isVip, score }) => ({ playerName, isVip, score }));
 
-  socket.on('reload-player', data => {
-    console.log('d', data);
-    const { gameCode, playerCode } = data;
+		// Send the game info back to the host
+		socket.emit('reload-host-accepted', { players });
+	});
 
-    // Confirm that the player is in the game
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    const player = game.players.find(player => player.playerCode === playerCode)
-    if (!player) {
-      console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('reload-player', (data) => {
+		console.log('d', data);
+		const { gameCode, playerCode } = data;
 
-    // Replace the player socket
-    player.socket = socket;
+		// Confirm that the player is in the game
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		const player = game.players.find((player) => player.playerCode === playerCode);
+		if (!player) {
+			console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
+			// TODO: send error down socket
+			return;
+		}
 
-    const { isVip, playerName, score } = player;
+		// Replace the player socket
+		player.socket = socket;
 
-    // Send the game info back to the host
-    socket.emit('reload-player-accepted', { isVip, playerName, score });
-  })
+		const { isVip, playerName, score } = player;
 
-  socket.on('request-round-start', data => {
-    const { gameCode, hostCode, players } = data;
+		// Send the game info back to the host
+		socket.emit('reload-player-accepted', { isVip, playerName, score });
+	});
 
-    // TODO: improve abstraction, this is the same logic as above
-    // Confirm that the host code is correct
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    if (game.hostCode !== hostCode) {
-      console.error('Host code incorrect', hostCode, game.hostCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('request-round-start', (data) => {
+		const { gameCode, hostCode, players } = data;
 
-    sessionsManager.updatePlayerScores(gameCode, players);
-    sessionsManager.startRound(gameCode);
-  })
+		// TODO: improve abstraction, this is the same logic as above
+		// Confirm that the host code is correct
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		if (game.hostCode !== hostCode) {
+			console.error('Host code incorrect', hostCode, game.hostCode);
+			// TODO: send error down socket
+			return;
+		}
 
-  socket.on('send-answers', data => {
-    const { answers, playerCode, gameCode } = data;
-    console.log(answers);
+		sessionsManager.updatePlayerScores(gameCode, players);
+		sessionsManager.startRound(gameCode);
+	});
 
-    // TODO: DRY this out
-    // Confirm that the player is in the game
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    const player = game.players.find(player => player.playerCode === playerCode)
-    if (!player) {
-      console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('send-answers', (data) => {
+		const { answers, playerCode, gameCode } = data;
+		console.log(answers);
 
-    sessionsManager.submitAnswers(gameCode, playerCode, player.playerName, answers);
-  })
+		// TODO: DRY this out
+		// Confirm that the player is in the game
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		const player = game.players.find((player) => player.playerCode === playerCode);
+		if (!player) {
+			console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
+			// TODO: send error down socket
+			return;
+		}
 
-  socket.on("round-times-up", data => {
-    const { gameCode, hostCode } = data;
+		sessionsManager.submitAnswers(gameCode, playerCode, player.playerName, answers);
+	});
 
-    // TODO: improve abstraction, this is the same logic as above
-    // Confirm that the host code is correct
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    if (game.hostCode !== hostCode) {
-      console.error('Host code incorrect', hostCode, game.hostCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('round-times-up', (data) => {
+		const { gameCode, hostCode } = data;
 
-    sessionsManager.roundTimesUp(gameCode);
-  })
+		// TODO: improve abstraction, this is the same logic as above
+		// Confirm that the host code is correct
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		if (game.hostCode !== hostCode) {
+			console.error('Host code incorrect', hostCode, game.hostCode);
+			// TODO: send error down socket
+			return;
+		}
 
-  socket.on("review-next", data => {
-    const { gameCode, hostCode, currentIndex } = data;
+		sessionsManager.roundTimesUp(gameCode);
+	});
 
-    // TODO: improve abstraction, this is the same logic as above
-    // Confirm that the host code is correct
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    if (game.hostCode !== hostCode) {
-      console.error('Host code incorrect', hostCode, game.hostCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('review-next', (data) => {
+		const { gameCode, hostCode, currentIndex } = data;
 
-    console.log('host: review-next')
-    sessionsManager.reviewNext(gameCode, currentIndex);
-  })
+		// TODO: improve abstraction, this is the same logic as above
+		// Confirm that the host code is correct
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		if (game.hostCode !== hostCode) {
+			console.error('Host code incorrect', hostCode, game.hostCode);
+			// TODO: send error down socket
+			return;
+		}
 
-  socket.on('mark-answer', data => {
-    const { mark, playerCode, gameCode } = data;
+		console.log('host: review-next');
+		sessionsManager.reviewNext(gameCode, currentIndex);
+	});
 
-    // TODO: DRY this out
-    // Confirm that the player is in the game
-    const game = sessionsManager.getGame(gameCode);
-    if (!game) {
-      console.error('Game not found', gameCode);
-      // TODO: send error down socket
-      return;
-    }
-    const player = game.players.find(player => player.playerCode === playerCode)
-    if (!player) {
-      console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
-      // TODO: send error down socket
-      return;
-    }
+	socket.on('mark-answer', (data) => {
+		const { mark, playerCode, gameCode } = data;
 
-    sessionsManager.markAnswer(gameCode, playerCode, mark);
-  })
+		// TODO: DRY this out
+		// Confirm that the player is in the game
+		const game = sessionsManager.getGame(gameCode);
+		if (!game) {
+			console.error('Game not found', gameCode);
+			// TODO: send error down socket
+			return;
+		}
+		const player = game.players.find((player) => player.playerCode === playerCode);
+		if (!player) {
+			console.error('Player code incorrect. gameCode, playerCode', gameCode, playerCode);
+			// TODO: send error down socket
+			return;
+		}
 
-  socket.on('disconnect', () => {
-    const player = sessionsManager.getPlayerBySocketId(socket.id)
+		sessionsManager.markAnswer(gameCode, playerCode, mark);
+	});
 
-    console.log('User disconnected', player)
-  })
+	socket.on('disconnect', () => {
+		const player = sessionsManager.getPlayerBySocketId(socket.id);
+
+		console.log('User disconnected', player);
+	});
 });
